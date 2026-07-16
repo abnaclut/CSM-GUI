764505efe3f7cf1112dbe6955908d7962e74485b
abnaclut <abnaclut@gmail.com>
2026-07-16 18:48:29 +0100

    test version

--- include/csm_gui/gui_cmd_output.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by 1111 on 15.07.2026.
-//
-
-#ifndef GUI_CMD_OUTPUT_HPP
-#define GUI_CMD_OUTPUT_HPP
-
-#endif //GUI_CMD_OUTPUT_HPP
--- src/gui_cmd_output.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 1111 on 15.07.2026.
-//
-
-#include "gui_cmd_output.hpp"
